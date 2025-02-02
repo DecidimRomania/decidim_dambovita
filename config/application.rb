3115972eec9ce52c1015b35a11e09475191951b4
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-02-02 02:22:00 +0200

    Configure Puma

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module DecidimDambovita
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    config.hosts << "decidem.cjd.ro"
   end
 end
