881cf250d27c8464c136a8ab10c322543a6a3e01
Alexandru Emil Lupu <contact@alecslupu.ro>
2025-02-02 02:26:43 +0200

    Configure queue adapter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.active_job.queue_adapter = ENV['QUEUE_ADAPTER'] if ENV['QUEUE_ADAPTER'].present?
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
